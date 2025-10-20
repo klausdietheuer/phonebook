64d16be26618ad7564a24d7409214227a4ed7711
dietheuer <dietheuer@staatsanzeiger-eservices.de>
2025-10-20 15:33:59 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.static('dist'))
 app.use(express.json())
 const morgan = require('morgan')
 morgan.token('body', (req) => JSON.stringify(req.body))
