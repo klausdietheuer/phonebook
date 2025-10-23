98c810485640f54beedbf701aef2f655606a5c52
Klaus Dietheuer <dietheuer@staatsanzeiger-online.de>
2025-10-23 15:55:40 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,44 +1,50 @@
+require('dotenv').config()
 const express = require('express')
+const person = require('./models/person')
 const app = express()
 app.use(express.static('dist'))
 app.use(express.json())
+
 const morgan = require('morgan')
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     {
+//         "id": "1",
+//         "name": "Arto Hellas",
+//         "number": "040-123456"
+//     },
+//     {
+//         "id": "2",
+//         "name": "Ada Lovelace",
+//         "number": "39-44-5323523"
+//     },
+//     {
+//         "id": "3",
+//         "name": "Dan Abramov",
+//         "number": "12-43-234345"
+//     },
+//     {
+//         "id": "4",
+//         "name": "Mary Poppendieck",
+//         "number": "39-23-6423122"
+//     }
+// ]
 
-const generateId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(n => Number(n.id)))
-        : 0
-    return String(maxId + 1)
-}
+// const generateId = () => {
+//     const maxId = persons.length > 0
+//         ? Math.max(...persons.map(n => Number(n.id)))
+//         : 0
+//     return String(maxId + 1)
+// }
 
-app.get('/info', (req, res) => {
+app.get('api/info', (req, res) => {
+    Person.find({}).then(people => {
+        res.json(people)
+    })
     const now = new Date();
-    const count = persons.length;
+    const count = people.length;
     const infotext = `
         <html lang="en">
             <body>
@@ -49,51 +55,49 @@ app.get('/info', (req, res) => {
     res.send(infotext);
 })
 
-app.get('/api/persons/', (req, res) => {
-    res.json(persons)
+app.get('/api/people/', (req, res) => {
+    Person.find({}).then(people => {
+        res.json(people)
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    if (person) {
+app.get('/api/people/:id', (request, response) => {
+    Person.findById(request.params.id).then(person => {
         response.json(person)
-    } else {
-        response.statusMessage = 'Person not found'
-        response.status(404).end()
-    }
+    })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/people', (request, response) => {
     const body = request.body
     if (!body.name || !body.number) {
         return response.status(400).json({ error: 'name and number is required' })
     }
-    if (persons.some(person => person.name === body.name) ) {
-        return response.status(400).json({ error: 'name is already in use' })
-    }
-    const person = {
+    // if (persons.some(person => person.name === body.name) ) {
+    //     return response.status(400).json({ error: 'name is already in use' })
+    // }
+    const person = new Person({
         name: body.name,
-        number: body.number || '',
-        id: generateId()
-    }
-    persons = persons.concat(person)
-    response.json(person)
+        number: body.number || ''
+    })
+
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
-})
+// app.delete('/api/persons/:id', (request, response) => {
+//     const id = request.params.id
+//     persons = persons.filter(person => person.id !== id)
+//
+//     response.status(204).end()
+// })
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
