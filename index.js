06b94ba4919769652d892bcfb7a444705cdb1b87
Klaus Dietheuer <dietheuer@staatsanzeiger-online.de>
2025-10-27 09:31:41 +0100

    uibuild

--- index.js
+++ index.js
@@ -92,7 +92,6 @@ app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
-
     if (error.name === 'CastError') {
         return response.status(400).send({error: 'malformatted id'})
     }
